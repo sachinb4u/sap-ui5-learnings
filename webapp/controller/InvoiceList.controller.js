d27748227850e768b3fa3077458b8487c91019ec
Sachin Bhosale <s.bhosale@sap.com>
2020-03-27 16:41:22 +0530

    Walkthrough Step 31 Routing and Navigation

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -28,6 +28,11 @@ sap.ui.define([
             var oList = this.byId("invoicesList");
             var oBinding = oList.getBinding("items");
             oBinding.filter(aFilter);
+        },
+
+        onPress : function(oEvent){
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("detail");
         }
     })
  
