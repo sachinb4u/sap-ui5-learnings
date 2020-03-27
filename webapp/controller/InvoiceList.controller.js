a6cba1e62d62a15ac45866d5d9d91fd27cf664a0
Sachin Bhosale <s.bhosale@sap.com>
2020-03-27 17:51:19 +0530

    Walkthrough Step 32 Routing with Parameters

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -31,8 +31,11 @@ sap.ui.define([
         },
 
         onPress : function(oEvent){
+            var oItem = oEvent.getSource();
             var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			oRouter.navTo("detail");
+			oRouter.navTo("detail", {
+                invoicePath : oItem.getBindingContext("invoices").getPath().substr(10)
+            });
         }
     })
  
